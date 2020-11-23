42533a373a15f888ce9030e2d4fc823a99305420
HugoPOuet <gosse_go@hotmail.fr>
2020-11-23 11:56:33 +0100

    dazddlsdfppokpokplkpokqfdsdfqsdfxdghsdfg

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ var cacheName = "ginko-v1";
 var contentToCache = [];
 var icons=["32","64","96","128","168","180","192","256","512"];
 
-for(var i=0; i<icons.length; i++){
+for(var i=0; i<icons.length; i++) {
     contentToCache.push('/icons/icon-'+icons[i]+'.png');
 }
 
@@ -12,16 +12,17 @@ contentToCache.push('app.js');
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(
-      caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching all: app shell and content');
-        return cache.addAll(contentToCache);
-      })
-    );
+        caches.open(cacheName).then((cache) => {
+              console.log('[Service Worker] Caching all: app shell and content');
+          return cache.addAll(contentToCache);
+        })
+      );
 });
+
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-      caches.open('mysite-dynamic').then(function (cache) {
+      caches.open(cacheName).then(function (cache) {
         return cache.match(event.request).then(function (response) {
           return (
             response ||
