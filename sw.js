c96ce90d88ce408b94be89f13506d60fa3638a81
HugoPOuet <gosse_go@hotmail.fr>
2020-11-23 11:30:27 +0100

    dazddlsdfp

---
+++ sw.js
@@ -0,0 +1,34 @@
+var cacheName = "ginko-v1";
+var contentToCache = [];
+var icons=["32","64","96","128","168","180","192","256","512"];
+
+for(var i=0; i<icons.length; i++){
+    contentToCache.push('/icons/icon-'+icons[i]+'.png');
+}
+
+contentToCache.push('index.html');
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(contentToCache);
+      })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: '+e.request.url);
+        return r || fetch(e.request).then((response) => {
+                  return caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+  });
