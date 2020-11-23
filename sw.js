2f1d9060c1c4caea67ea0180410c5dc774a9dffa
HugoPOuet <gosse_go@hotmail.fr>
2020-11-23 11:53:08 +0100

    dazddlsdfppokpokplkpokqfdsdfqsd

--- sw.js
+++ sw.js
@@ -19,17 +19,18 @@ self.addEventListener('install', (e) => {
     );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-        return r || fetch(e.request).then((response) => {
-                  return caches.open(cacheName).then((cache) => {
-            console.log('[Service Worker] Caching new resource: '+e.request.url);
-            cache.put(e.request, response.clone());
-            return response || fetchPromise;
-          });
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+      caches.open('mysite-dynamic').then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          return (
+            response ||
+            fetch(event.request).then(function (response) {
+              cache.put(event.request, response.clone());
+              return response;
+            })
+          );
         });
-      })
+      }),
     );
   });
